b551ff410ae3193d83663e2faf8c9393f0cb536d
Rawat Pallavi <me@rawatpallavi.com>
2020-04-12 14:17:39 +0530

    add client and try client logging

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,12 @@ dependencies {
     implementation("io.ktor:ktor-server-netty:$ktor_version")
     implementation("io.ktor:ktor-network-tls-certificates:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("io.ktor:ktor-client-apache:$ktor_version")
+    implementation("io.ktor:ktor-client-cio:$ktor_version")
+    implementation("io.ktor:ktor-client-json:$ktor_version")
+    implementation("io.ktor:ktor-client-gson:$ktor_version")
+    implementation("io.ktor:ktor-client-logging:$ktor_version")
+    implementation("io.ktor:ktor-client-logging-jvm:$ktor_version")
     testImplementation("io.ktor:ktor-server-tests:$ktor_version")
 }
 
--- src/Application.kt
+++ src/Application.kt
@@ -1,12 +1,23 @@
 package com.example
 
-import io.ktor.application.*
-import io.ktor.network.tls.certificates.generateCertificate
-import io.ktor.response.*
-import io.ktor.request.*
+import io.ktor.application.Application
+import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.apache.Apache
+import io.ktor.client.features.json.GsonSerializer
+import io.ktor.client.features.json.JsonFeature
+import io.ktor.client.features.logging.DEFAULT
+import io.ktor.client.features.logging.LogLevel
+import io.ktor.client.features.logging.Logger
+import io.ktor.client.features.logging.Logging
+import io.ktor.client.request.get
+import io.ktor.features.CallLogging
+import io.ktor.response.respondText
 import io.ktor.routing.get
 import io.ktor.routing.routing
-import java.io.File
+import kotlinx.coroutines.runBlocking
+import org.slf4j.event.Level
 
 fun main(args: Array<String>): Unit {
     println("heya")
@@ -16,19 +27,41 @@ fun main(args: Array<String>): Unit {
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
+    install(CallLogging) {
+        level = Level.INFO
+        // in presence of filters only these get logged
+//        filter { call -> call.request.path().startsWith("/section1") }
+//        filter { call -> call.request.path().startsWith("/section2") }
+        // ...
+    }
+
     routing {
-        get("/health_check") {
+        get("/health") {
             call.respondText("OK")
         }
-    }
-}
 
-fun generateCert(){
-    val jksFile = File("buildcert/temporary.jks").apply {
-        parentFile.mkdirs()
-    }
+        get("/client") {
+            runBlocking {
+                val client = HttpClient(Apache) {
+                    install(JsonFeature) {
+                        serializer = GsonSerializer {
+                            // .GsonBuilder
+                            serializeNulls()
+                            disableHtmlEscaping()
+                        }
+                    }
+                    install(Logging, fun Logging.Config.() {
+                        logger = Logger.DEFAULT
+                        level = LogLevel.INFO
+                    })
+                }
 
-    if (!jksFile.exists()) {
-        generateCertificate(jksFile) // Generates the certificate
+                val message = client.get<String>("http://127.0.0.1:8080/healths")
+                println("CLIENT: Message from the server: $message")
+
+                client.close()
+            }
+            call.respondText("OK")
+        }
     }
 }
